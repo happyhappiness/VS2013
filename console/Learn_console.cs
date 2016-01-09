401034d2cae23bd40e2f69d06116b69d9d124b82
happyhappiness <993273596@qq.com>
2016-01-09 14:51:49 +0800

    异常处理

--- console/Learn_console.cs
+++ console/Learn_console.cs
@@ -3,15 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace console
 {
+
     class Learn_console
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("这是第一个c#学习日欧， 干巴嗲");
-            Console.ReadKey();
+            string eType;
+            while(true)
+            {
+                Console.WriteLine("请输入想要抛出的异常类型");
+                eType = Console.ReadLine();
+                try
+                {
+                    ThrowException(eType);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.ToString());
+                    Console.WriteLine("catch index out of range");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    Console.WriteLine("catch");
+                }
+                finally
+                {
+                    Console.WriteLine("到finally了偶");
+                }
+            }
+
+        }
+
+    private static void ThrowException(string eType)
+    {
+        switch (eType)
+        { 
+            case "none":
+                //donothing
+                break;
+            case "index":
+                throw new IndexOutOfRangeException();
+                break;
+            case "nested index":
+                try
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("nested index catch");
+                    e.ToString();
+                    throw;
+                }
+                finally
+                {
+                    Console.WriteLine("nested index finally");
+                }
+                break;
+            case "simple":
+                throw new SystemException();
+                break;
         }
     }
+    }
 }
+
